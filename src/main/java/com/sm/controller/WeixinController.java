a8a7bd17a378746ef6863ce1577e256a4f4a4a45
longkai.zhao <zhaolongkai3k@gmail.com>
2015-03-07 22:42:58 +0800

    fix menu click

--- src/main/java/com/sm/controller/WeixinController.java
+++ src/main/java/com/sm/controller/WeixinController.java
@@ -6,6 +6,8 @@ import com.github.sd4324530.fastweixin.message.Article;
 import com.github.sd4324530.fastweixin.message.BaseMsg;
 import com.github.sd4324530.fastweixin.message.NewsMsg;
 import com.github.sd4324530.fastweixin.message.TextMsg;
+import com.github.sd4324530.fastweixin.message.req.BaseEvent;
+import com.github.sd4324530.fastweixin.message.req.MenuEvent;
 import com.github.sd4324530.fastweixin.message.req.TextReqMsg;
 import com.google.common.collect.Lists;
 import com.sm.entity.Category;
@@ -110,10 +112,61 @@ public class WeixinController extends WeixinControllerSupport {
         }
         return new TextMsg("服务器回复用户消息!");
     }
+
+    @Override
+    protected BaseMsg handleMenuClickEvent(MenuEvent event) {
+        String keyEvent = event.getEventKey();
+        //产品资料
+        if (keyEvent.equals("products")) {
+            List<Article> articles = Lists.newArrayList();
+            List<Category> category = categoryRepository.findByType("products");
+            if (!CollectionUtils.isEmpty(category)) {
+                Article article = new Article();
+                article.setPicUrl(category.get(0).getThumbnail());
+                article.setTitle("产品资料");
+                article.setUrl(host + "/web/productIntro");
+                article.setDescription("点击查看更多产品资料");
+                articles.add(article);
+                return new NewsMsg(articles);
+            }
+        }else if(keyEvent.equals("schemes")||keyEvent.equals("cases")||keyEvent.equals("demos")){
+            //解决方案、案例、demo
+            List<Category> categorys = categoryRepository.findByType(keyEvent);
+            List<Article> articles = Lists.newArrayList();
+            Article article = null;
+            for(Category category:categorys){
+                article = new Article();
+                article.setPicUrl(category.getThumbnail());
+                article.setTitle(category.getName());
+                article.setUrl(host+"/web/resourceList?categoryId="+category.getId());
+                articles.add(article);
+            }
+            return new NewsMsg(articles);
+        }else if(keyEvent.equals("videos")){
+            List<Video> videos = videoRepository.findAll();
+            List<Article> articles = Lists.newArrayList();
+            Article article = null;
+            Video video = videos.get(0);
+            if (video != null) {
+                article = new Article();
+                article.setPicUrl(video.getLogo());
+                article.setTitle(video.getName());
+                article.setUrl(host + "/web/videoList");
+                article.setDescription("点击查看更多在线视频");
+                articles.add(article);
+            }
+            return new NewsMsg(articles);
+        }else if(keyEvent.equals("about")){
+
+            return new TextMsg("关于").addLink("点击跳转", host+"/web/about");
+        }
+        return new TextMsg("服务器回复用户消息!");
+    }
+
     /*1.1版本新增，重写父类方法，加入自定义微信消息处理器
-     *不是必须的，上面的方法是统一处理所有的文本消息，如果业务觉复杂，上面的会显得比较乱
-     *这个机制就是为了应对这种情况，每个MessageHandle就是一个业务，只处理指定的那部分消息
-     */
+         *不是必须的，上面的方法是统一处理所有的文本消息，如果业务觉复杂，上面的会显得比较乱
+         *这个机制就是为了应对这种情况，每个MessageHandle就是一个业务，只处理指定的那部分消息
+         */
     @Override
     protected List<MessageHandle> initMessageHandles() {
         List<MessageHandle> handles = new ArrayList<MessageHandle>();
